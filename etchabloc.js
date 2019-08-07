7e7c664aeeb1af1d452f276e9afca3fb402aac69
vonnenaut <danielashcom@gmail.com>
2019-08-07 13:15:22 +0700

    Fixed grid issue in etchabloc.js

--- etchabloc.js
+++ etchabloc.js
@@ -6,28 +6,19 @@ function etchABloc() {
 	var element = document.createElement('style'), sheet;
 	document.head.appendChild(element);
 	this.sheet = element.sheet;
-	this.height = 16;
+	this.height = 16; // number of squares in grid
 	this.width = 16;
+	this.screenWidth = 500; // size of screen itself
+	this.screenHeight = 350;
+	this.screen = undefined;
 	init();
 
 	function init() {
-		let box = 1;
-		var screen = document.getElementById('screen');
-		
-		for(var idx = 1; idx <= this.height; idx++) {
-			for(var jdx = 1; jdx <= this.width; jdx++) {
-				// create css style grid
-				let selector = '.grid' + box
-				let rules = 'grid-column: col ' + jdx +
-					' / span 1; grid-row: row ' + idx + ' / span 1;'
-				addCSSRules(selector, rules, this.sheet.cssRules.length);
-				
-				// create html grid
-				addHTMLGrid(screen, box);
-
-				box += 1;
-			}
-		}
+		// initialize and set up screen
+		this.screen = initScreen(this.height, this.width, this.screenHeight, this.screenWidth);
+		//add css grid and html grid to screen
+		createGrid();
+		//set up event listeners
 	}
 
 	function addHTMLGrid(selector, boxNumber) {
@@ -44,6 +35,39 @@ function etchABloc() {
 			this.sheet.insertRule(selector + " { " + rules + " }", index);
 		} else {
 			console.log("addCSSRules:  stylesheet undefined!");
+		}
+	}
+
+	function initScreen(height, width, screenh, screenw) {		
+		let screen = document.getElementById('screen');
+		let boxWidth = screenw / width;
+		let boxHeight = screenh / height;
+		console.log("boxWidth: ", boxWidth);
+		console.log("boxHeight: ", boxHeight);
+
+		// add css for screen's grid
+		addCSSRules('#screen', 'width: ${screenw}px; height: 350px; background-color: rgb(212,208,207); margin: 0 auto; border: 1px solid black; border-radius: 15px; display: grid; grid-template-rows: repeat(${height}) 1fr; grid-template-columns: repeat(${width}) 1fr; grid-gap: 0;', this.sheet.cssRules.length);
+
+		// add css for box class inside screen container
+		addCSSRules('.box', 'width: ${boxWidth}px; height: ${boxHeight}; border: 1px solid rgb(107,107,107); padding: 0; margin: 0;');
+		return screen;
+	}
+
+	function createGrid() {
+		let box = 0; // counter for labeling box id's
+
+		for(var idx = 0; idx < this.height; idx++) {
+			for(var jdx = 0; jdx < this.width; jdx++) {
+				// create css grid
+				let selector = '.grid' + box
+				let rules = 'grid-column: col ' + jdx +
+					' / span 1; grid-row: row ' + idx + ' / span 1;'
+				addCSSRules(selector, rules, this.sheet.cssRules.length);
+				
+				// create html grid
+				addHTMLGrid(screen, box);
+				box += 1;
+			}
 		}
 	}
 	
