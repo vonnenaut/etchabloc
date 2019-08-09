b9ca2a707348f7ad6c614588436a0b4d444e3c96
vonnenaut <danielashcom@gmail.com>
2019-08-09 11:26:18 +0700

    Refactored for easier reading and to simplify code -- resolved some issues

--- etchabloc.js
+++ etchabloc.js
@@ -5,33 +5,54 @@ const etchABloc = {
 	///// Initializer //////////////////////////////////
 	init: function(dimension) {
 		/* variable definitions */
-		this.dimension = 16; // number of grid squares on a side
+		this.dimension = dimension; // number of grid squares on a side
 		this.screenWidth = 500; // size of screen
 		this.screenHeight = 350;
-		this.screen = undefined;
-		this.sheet = undefined;
+		this.screen = document.getElementById('screen');;
+		this.sheet = this.addStyleSheet();
 		this.resetListener = undefined;
 		this.modeListener = undefined;
 
-		// initialize and set up screen
-		this.screen = this.initScreen(this.dimension, this.screenHeight, this.screenWidth);
-		//add css grid and html grid to screen
-		this.createGrid();
+		// set up grid
+		this.addGrid(this.screen, this.dimension, this.screenHeight, this.screenWidth);
+
 		// add event listeners
 		this.addEventListeners();
 	},
 
 
+	///// Button-handling////////////////////////////////
+	reset: function() {
+		this.sheet = this.addStyleSheet;	// initialize new stylesheet for new grid
+		var input = 0;	
+
+		// get user input for grid resolution
+		while(input <= 0 || input > 64) {
+			input = window.prompt("Number of squares per side (between 1 and 64):");
+		} 
+
+		// set grid variables and reinitialize grid
+		this.dimension = input;
+
+		this.addGrid(this.dimension, this.screenHeight, this.screenWidth);
+		// this.addEventListeners();
+	},
+
+	mode: function() {},
+
+
 	///// Methods /////////////////////////////////////
-	initScreen: function(dimension, screenh, screenw) {
-		let screen = document.getElementById('screen');
+	addGrid: function(screen, dimension, screenh, screenw) {
 		let boxWidth = screenw / dimension;
 		let boxHeight = screenh / dimension;
+		let box = 0; // counter for labeling box id's
 
-		// create css for current grid layout
-		this.addStyleSheet();
+		// remove existing HTML grid divs
+		screen.innerHTML = '';
 
-		// add css for screen's grid -- moved this to css using variable '--dimension'
+		//remove existing dynamic css and set css variable to new grid dimensions
+		// document.styleSheetList[]
+			// add css for screen's grid -- moved this to css using variable '--dimension'
 		// addCSSRules('#screen', 'width: ${screenw}px; height: 350px; background-color: rgb(212,208,207); margin: 0 auto; border: 1px solid black; border-radius: 15px; display: grid; grid-template-rows: repeat(${dimension}) 1fr; grid-template-columns: repeat(${dimension}) 1fr; grid-gap: 0;', this.sheet.cssRules.length);
 		document.documentElement.style.setProperty('--dimension', dimension);
 
@@ -39,23 +60,13 @@ const etchABloc = {
 		this.addCSSRules('.box', 'width: ${boxWidth}px; height: ${boxHeight}; border: 1px solid rgb(107,107,107); padding: 0; margin: 0;');
 		// should probably add housekeeping to delete old css sheet when resizing grid --  this should also be along the lines of getting functionality to work as well
 		console.log("document.styleSheets: ", document.styleSheets);
-		console.log("Deleting old sheet...")
+		// console.log("Deleting old sheet...")
 		// document.stylesheets
 		// console.log
 
-		return screen;
-	},
-
-
-	createGrid: function() {
-		let box = 0; // counter for labeling box id's
-
-		// remove existing HTML grid divs
-		this.screen.innerHTML = '';
-
-		if(this.dimension > 0 && this.dimension < 65) {
-			for(var idx = 0; idx < this.dimension; idx++) {
-				for(var jdx = 0; jdx < this.dimension; jdx++) {
+		if(dimension > 0 && dimension < 65) {
+			for(var idx = 0; idx < dimension; idx++) {
+				for(var jdx = 0; jdx < dimension; jdx++) {
 					// create css grid
 					let selector = '.grid' + box
 					let rules = 'grid-column: col ' + jdx +
@@ -71,6 +82,18 @@ const etchABloc = {
 	},
 
 
+	///// Helpers //////////////////////////////////////////
+	addEventListeners: function() {
+		this.screen.addEventListener("mouseover", function( event ) {
+			event.target.style.background = "black";
+		})
+		// add reset and mode button event listeners
+		var resetBtn = document.getElementById('reset');
+		var modeBtn = document.getElementById('mode');
+		this.resetListener = resetBtn.addEventListener("click", this.reset);
+		this.modeListener = modeBtn.addEventListener("click", function() { console.log('mode clicked'); });
+	},
+
 	addCSSRules: function(selector, rules, index) {
 		if(typeof this.sheet !== 'undefined') {
 			this.sheet.insertRule(selector + " { " + rules + " }", index);
@@ -78,7 +101,6 @@ const etchABloc = {
 			console.log("addCSSRules:  stylesheet undefined!");
 		}
 	},
-
 
 	addHTMLGrid: function(selector, boxNumber) {
 		let construct = '<div class=\"box grid' + boxNumber + '\"></div>'
@@ -89,50 +111,17 @@ const etchABloc = {
 		}
 	},
 
-
-	addEventListeners: function() {
-		this.screen.addEventListener("mouseover", function( event ) {
-			event.target.style.background = "black";
-		})
-		// add reset button event listener
-		let resetBtn = document.getElementById('reset');
-		let modeBtn = document.getElementById('mode');
-		this.resetListener = resetBtn.addEventListener("click", function(){reset()});
-		this.modeListener = modeBtn.addEventListener("click", function(){console.log('mode clicked')});
-	},
-
-
-	reset: function() {
-		boxArray = document.getElementsByClassName('box');
-
-		// reset screen boxes to background color
-		for(idx=0; idx <boxArray.length; idx++) {
-			boxArray[idx].style.background = "rgb(212,208,207)";
-		};
-
-		// get user input for grid resolution		
-		var input = 0;		
-
-		while(input <= 0 || input > 64) {
-			input = window.prompt("Number of squares per side (between 1 and 64):");
-		} 
-
-		// set grid variables and reinitialize grid
-		this.height = input;
-		this.width = this.height;
-
-		this.screen = initScreen(this.height, this.width, this.screenHeight, this.screenWidth);
-		this.createGrid();
-		this.addEventListeners();
-	},
-
-
-	///// Helpers //////////////////////////////////////////
 	addStyleSheet: function() {
-		var element = document.createElement('style'), sheet;
+		// // TO-DO:  If there are any sheets created by this script, remove them before creating another
+		// while(document.styleSheets.length > 2) {
+		// 	delete document.styleSheets[document.styleSheets.length-1];
+		// }
+		let element = document.createElement('style');
 		document.head.appendChild(element);
-		this.sheet = element.sheet;
+		return element.sheet;
 	}
 };
 
-window.onload = etchABloc.init(16);
+
+const etch = Object.create(etchABloc);
+window.onload = etch.init(16);
