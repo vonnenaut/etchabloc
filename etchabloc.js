12b25e31c5b24c435f219ec4d4b5f0c1e5b89150
vonnenaut <danielashcom@gmail.com>
2019-08-12 17:53:12 +0700

    Improved reset functionality by moving some of etchabloc.js code outside of object and calling externally

--- etchabloc.js
+++ etchabloc.js
@@ -11,65 +11,35 @@ const etchABloc = {
 		this.screenHeight = 350;
 		this.screen = document.getElementById('screen');;
 		this.sheet = this.addStyleSheet();
-		this.resetListener = undefined;
-		this.modeListener = undefined;
 
 		// set up grid
-		this.addGrid(this.screen, this.dimension, this.screenHeight, this.screenWidth);
-
-		// add event listeners
-		this.addEventListeners();
+		this.addGrid();
 	},
-
-
-	///// Button-handling////////////////////////////////
-	reset: function() {
-		/* resets object upon user input
-		*/
-		this.sheet = this.addStyleSheet;	// initialize new stylesheet for new grid
-		var input = 0;	
-
-		// get user input for grid resolution
-		while(input <= 0 || input > 64) {
-			input = window.prompt("Number of squares per side (between 1 and 64):");
-		} 
-
-		// set grid variables and reinitialize grid
-		this.dimension = input;
-
-		this.addGrid(this.screen, this.dimension, this.screenHeight, this.screenWidth);
-		// this.addEventListeners();
-	},
-
-	mode: function() {},
 
 
 	///// Methods /////////////////////////////////////
-	addGrid: function(screen, dimension, screenh, screenw) {
+	addGrid: function() {
 		/* adds grid HTML and CSS to screen container 
 		with help of reinitGrid
 		*/
 		let box = 0; // counter for labeling box id's
 		
-		this.reinitGrid(dimension, screenw, screenh);
+		this.reinitGrid(this.dimension, this.screenWidth, this.screenHeight);
 
-		if(dimension > 0 && dimension < 65) {
-			for(var idx = 0; idx < dimension; idx++) {
-				for(var jdx = 0; jdx < dimension; jdx++) {
-					// create css grid
-					let selector = '.grid' + box
-					let rules = 'grid-column: col ' + jdx +
-						' / span 1; grid-row: row ' + idx + ' / span 1;'
-					this.addCSSRules(selector, rules, this.sheet.cssRules.length);
-					
-					// create html grid
-					this.addHTMLGrid(screen, box);
-					box += 1;
-				}
+		for(var idx = 0; idx < this.dimension; idx++) {
+			for(var jdx = 0; jdx < this.dimension; jdx++) {
+				// create css grid
+				let selector = '.grid' + box
+				let rules = 'grid-column: col ' + jdx +
+					' / span 1; grid-row: row ' + idx + ' / span 1;'
+				this.addCSSRules(selector, rules, this.sheet.cssRules.length);
+				
+				// create html grid
+				this.addHTMLGrid(this.screen, box);
+				box += 1;
 			}
 		}
 	},
-
 
 	reinitGrid: function(dimension, width, height) {
 		/* 	Helper function for addGrid
@@ -82,32 +52,19 @@ const etchABloc = {
 		
 
 		// remove existing HTML grid divs
-		screen.innerHTML = '';
+		this.screen.innerHTML = '';
 
 		// update css variable to new user-input grid size
 		document.documentElement.style.setProperty('--dimension', dimension);
 
-		// add css for box class inside screen container
+		// add css for box class
 		this.addCSSRules('.box', 'width: ${boxWidth}px; height: ${boxHeight}; border: 1px solid rgb(107,107,107); padding: 0; margin: 0;');
 		// for troubleshooting:
-		console.log("document.styleSheets: ", document.styleSheets);
+		// console.log("document.styleSheets: ", document.styleSheets);
 	},
 
 
 	///// Helpers //////////////////////////////////////////
-	addEventListeners: function() {
-		/* 	Adds event listeners to the two buttons,
-			'Reset' and 'Mode'*/
-		this.screen.addEventListener("mouseover", function( event ) {
-			event.target.style.background = "black";
-		})
-		// add reset and mode button event listeners
-		var resetBtn = document.getElementById('reset');
-		var modeBtn = document.getElementById('mode');
-		this.resetListener = resetBtn.addEventListener("click", this.reset);
-		this.modeListener = modeBtn.addEventListener("click", function() { console.log('mode clicked'); });
-	},
-
 	addCSSRules: function(selector, rules, index) {
 		/*  Adds CSS rules to JS-created dynamic stylesheet
 		*/
@@ -139,5 +96,46 @@ const etchABloc = {
 };
 
 
-const etch = Object.create(etchABloc);
-window.onload = etch.init(16);
+function createObj(dimension) {
+	init(dimension);
+
+	function init(dimension) {
+		// create object and call its init method, add event listeners for the two buttons
+		const etch = Object.create(etchABloc);
+		etch.init(dimension);
+		addEventListeners();
+
+		///// Button-handling /////
+		function addEventListeners() {
+		/* 	Adds event listeners to the two buttons,
+			'Reset' and 'Mode'*/
+		etch.screen.addEventListener("mouseover", function( event ) {
+			event.target.style.background = "black";
+		});
+		// add reset and mode button event listeners
+		let resetBtn = document.getElementById('reset');
+		let modeBtn = document.getElementById('mode');
+		etch.resetListener = resetBtn.addEventListener("click", reset);
+		etch.modeListener = modeBtn.addEventListener("click", function() { alert('mode functionality disabled!'); });
+		}
+
+		function reset() {
+			/* resets object upon user input
+			*/
+			let input = 0;
+
+			// get user input for grid resolution
+			while(input < 8 || input > 64) {
+				input = window.prompt("Enter number of squares per side, between 8 and 64:");
+			} 
+
+			// init object
+			init(input);
+		}
+	}
+
+}
+
+window.onload = function() { 
+	createObj(16);
+}
